7180a2570e068c3d92a94e35832a22b092a3fdc5
Howell <th15817161961@gmail.com>
2016-11-15 21:32:44 +0800

    密码控件jsp改成Response HTML

--- bppe-portal-resources/bppe-portal-web/src/main/java/cn/com/bestpay/portal/controller/HTMLController.java
+++ bppe-portal-resources/bppe-portal-web/src/main/java/cn/com/bestpay/portal/controller/HTMLController.java
@@ -1,20 +1,22 @@
 package cn.com.bestpay.portal.controller;
 
 
+import cn.com.bestpay.portal.SecurityScript.SecurityHTML;
+import cn.com.bestpay.portal.View.HbsViewResolver;
 import cn.com.bestpay.portal.pojo.AppcenterModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by yfzx_gd_yanghh on 2016/9/28.
@@ -23,6 +25,8 @@ import java.util.Map;
 public class HTMLController extends BaseController{
     private static Logger logger = LoggerFactory.getLogger(HTMLController.class);
 
+    @Autowired
+    SecurityHTML securityHTML;
 
     @RequestMapping(value = "/{First}.html", method = RequestMethod.GET)
     public ModelAndView First(@PathVariable("First")String First) {
@@ -68,9 +72,24 @@ public class HTMLController extends BaseController{
      * @return
      */
     @RequestMapping(value = "/webCommon/SecurityPassword/{First}/{Second}.hbs", method = RequestMethod.GET)
-    public ModelAndView getWebCommonView(@PathVariable("First")String First, @PathVariable("Second")String Second) {
+    @ResponseBody
+    public String getWebCommonView(@PathVariable("First")String First, @PathVariable("Second")String Second, HttpServletRequest request) {
         logger.info("getWebCommonView file :"+Second);
-        return new ModelAndView("forward:/webCommon/SecurityPassword/"+First+"/"+Second+".jsp");
+        //return new ModelAndView("forward:/webCommon/SecurityPassword/"+First+"/"+Second+".jsp");
+
+        String retHTML = securityHTML.getSecurityHTML("loginpwd",
+                "loginpwd",
+                "loginpwd",
+                "login-rd",
+                "login-pwd-ses-key",
+                false,
+                "pwdmark:0",
+                request
+        );
+
+        return retHTML;
     }
+
+
 
 }
